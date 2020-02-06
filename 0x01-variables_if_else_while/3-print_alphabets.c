83404d71af4b60b6a5a4bebe93278e1b0261d9e2
allan-io <allanoliveira.dev@gmail.com>
2020-02-06 19:37:25 +0000

    Source code for task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 /**
- *
- *
+ * main - main function
+ * Description: prints the alphabet in lowercase and uppercase
+ * only uses putchar and a maximum of 3 times
+ * Return: 0
  */
 int main(void)
 {
-	return ();
+	char *lower_alpha;
+	char *upper_alpha;
+	int i;
+
+	lower_alpha = "abcdefghijklmnopqrstuvwxyz";
+	upper_alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	i = 0;
+	while (i < 26)
+	{
+		putchar(lower_alpha[i]);
+		i++;
+	}
+	i = 0;
+	while (i < 26)
+	{
+		putchar(upper_alpha[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
