21cd59904d72d68b49adc8fb914792afa6c83c1c
allan-io <allanoliveira.dev@gmail.com>
2020-02-06 19:57:34 +0000

    Source code for task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 /**
- *
- *
+ * main - main function
+ * Description: print 0-9 using putchar only twice
+ * Return: 0
  */
 int main(void)
 {
-	return ();
+	int i;
+
+	i = 48;
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
