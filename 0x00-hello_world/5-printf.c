3bb4f6e22f21b79055e71901da86cd3601e1e5a4
allan-io <allanoliveira.dev@gmail.com>
2020-02-06 00:19:46 +0000

    Create C program that prints string with printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - printf a string
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
+
