deb3681db649dc5ca685ab46c32eca9de7cc80f2
allan-io <allanoliveira.dev@gmail.com>
2020-02-06 22:20:31 +0000

    Source code task 10

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,9 +1,33 @@
 #include <stdio.h>
 /**
- *
- *
+ * main - main function
+ * Description: prints numbers from 00 - 99 only putchar allowed
+ * numbers are separated by comma and space except number 99
+ * only used putchar 5 times and no char type used
+ * Return:0
  */
 int main(void)
 {
-	return ();
+	int i;
+	int j;
+
+	i = 48;
+	j = 48;
+	while (i < 58)
+	{
+		while (j < 58)
+		{
+			putchar(i);
+			putchar(j);
+			if (i == 57 && j == 57)
+				break;
+			putchar(44);
+			putchar(32);
+			j++;
+		}
+		j = 48;
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
