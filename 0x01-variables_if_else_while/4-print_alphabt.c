294247c36e8521f8ea60e7324d377dac4258b89c
allan-io <allanoliveira.dev@gmail.com>
2020-02-06 19:38:06 +0000

    Source code for task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 /**
- *
- *
+ * main - main function
+ * Description: prints lowercase alphabet minus q and e
+ * Return: 0
  */
 int main(void)
 {
-	return ();
+	char *alphabet;
+	int i;
+
+	alphabet = "abcdefghijklmnopqrstuvwxyz";
+	i = 0;
+	while (i < 26)
+	{
+		if (alphabet[i] != 'q' && alphabet[i] != 'e')
+			putchar(alphabet[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
