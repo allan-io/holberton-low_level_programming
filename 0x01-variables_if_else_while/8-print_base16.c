140f00d5913a715d6bce9b6b8f34f93abc677a29
allan-io <allanoliveira.dev@gmail.com>
2020-02-06 20:29:32 +0000

    Source code for task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 /**
- *
- *
+ * main - main function
+ * Description: print 0123456789abcdef using only printchar max 3 times
+ * Return: 0
  */
 int main(void)
 {
-	return ();
+	int i = 48;
+	int j = 97;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	while (j < 103)
+	{
+		putchar(j);
+		j++;
+	}
+	putchar('\n');
+	return (0);
 }
