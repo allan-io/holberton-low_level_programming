c36e528849b453d90c6bc95be9ba53ba9fa0ed6b
allan-io <allanoliveira.dev@gmail.com>
2020-02-06 21:08:29 +0000

    Source code for task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
 /**
- *
- *
+ * main - main function
+ * Description: print 1-9 with comma and space between each number
+ * only use putchar and maximum of 4 times
+ * no comma or space after the 9
+ * Return: 0
  */
 int main(void)
 {
-	return ();
+	int i;
+
+	i = 48;
+	while (i < 58)
+	{
+		putchar(i);
+		if (i < 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	i++;
+	}
+	putchar('\n');
+	return (0);
 }
