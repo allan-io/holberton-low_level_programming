8a695d85bfdb73fd2ebb221b602fec9cf2af4a55
allan-io <allanoliveira.dev@gmail.com>
2020-02-06 16:52:47 +0000

    Create program that uses if else statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
+
 /**
- *
- *
+ * main - check if random number is negative, positive, or zero
+ * Return: 0
  */
 int main(void)
 {
-	return ();
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n == 0)
+	{
+		printf("is zero\n");
+	}
+	else if (n > 0)
+	{
+		printf("is positive\n");
+	}
+	else
+	{
+		printf("is negative\n");
+	}
+	return (0);
 }
