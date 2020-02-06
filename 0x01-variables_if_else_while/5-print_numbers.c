aaa5e4a4b560d4cf2837ae6869dc02bcbb4d0019
allan-io <allanoliveira.dev@gmail.com>
2020-02-06 19:48:55 +0000

    Source code for task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 /**
- *
- *
+ * main - main function
+ * Description: print all single digit numbers of base 10
+ * Return: 0
  */
 int main(void)
 {
-	return ();
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
