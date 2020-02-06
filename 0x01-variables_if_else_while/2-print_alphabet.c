dccafd397605856a314c585185469b0556a6ba2a
allan-io <allanoliveira.dev@gmail.com>
2020-02-06 19:36:12 +0000

    Source code for task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 /**
- *
- *
+ * main - main function
+ * Description: print alphabet in lowercase using putchar
+ * Return: 0
  */
 int main(void)
 {
-	return ();
+	int i;
+	char *alphabet;
+
+	alphabet = "abcdefghijklmnopqrstuvwxyz";
+	i = 0;
+	while (i < 26)
+	{
+		putchar(alphabet[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
