1bd3d1f91127f40afe62863a891816530dc99c6b
allan-io <allanoliveira.dev@gmail.com>
2020-02-10 20:24:30 +0000

    Create program 1-alphabet.c
    - program prints lowercase alphabet using _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * print_alphabet - prints a to z
+ * Description: use only _putchar
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	int i = 'a';
+
+	while (i <= 'z')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
