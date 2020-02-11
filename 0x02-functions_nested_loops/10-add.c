aeaf25d305aafd4e348c653c8c4c04aa401bf7b7
allan-io <allanoliveira.dev@gmail.com>
2020-02-11 18:44:36 +0000

    Create 10-add.c program

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/* add - add two numbers a and b
+ * @a: integer type number
+ * @b: integer type number
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
