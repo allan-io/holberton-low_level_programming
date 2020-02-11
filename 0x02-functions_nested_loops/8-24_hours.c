69bddb495f3911c2b5378b8dde9f452f9e6f543b
allan-io <allanoliveira.dev@gmail.com>
2020-02-11 16:39:29 +0000

    Remove nested loops and add if statements instead

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,34 +5,33 @@
  */
 void jack_bauer(void)
 {
-	int i = 0, j, k, l;
+	int total_mins = 0, i = 0, j = 0, k = 0, l = 0;
 
-	while (i < 3)
+	while (total_mins < 1440)
 	{
-		j = 0;
-		while (j < 4)
+		_putchar(i + '0');
+		_putchar(j + '0');
+		_putchar(':');
+		_putchar(k + '0');
+		_putchar(l + '0');
+		_putchar('\n');
+		l++;
+
+		if (l > 9)
+		{
+			l = 0;
+			k++;
+		}
+		if (k > 5)
 		{
 			k = 0;
-			while (k < 6)
-			{
-				l = 0;
-				while (l < 10)
-				{
-					_putchar(i + '0');
-					_putchar(j + '0');
-					_putchar(':');
-					_putchar(k + '0');
-					_putchar(l + '0');
-					_putchar('\n');
-					/* program will end if time reads 23:59 */
-					if (i + j + k + l == 19)
-						break;
-					l++;
-				}
-				k++;
-			}
 			j++;
 		}
-		i++;
+		if (j > 9)
+		{
+			j = 0;
+			j++;
+		}
+		total_mins++;
 	}
 }
