1d18087c9fc8ee47fcdf9dc9990118e0dbea999c
allan-io <allanoliveira.dev@gmail.com>
2020-02-11 15:24:44 +0000

    Create 2-print_albhabet_x10 program

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - Print alphabet 10 times
+ * Description: alphabet is in lowercase and new line after each instance
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0, c;
+
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+	return (0);
+}
