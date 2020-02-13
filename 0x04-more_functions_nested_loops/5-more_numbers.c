5bc2b3db0f1fb6fc6d8c89af944fa48429bb7c74
allan-io <allanoliveira.dev@gmail.com>
2020-02-13 18:31:53 +0000

    Create 5-more_numbers.c program

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,11 +1,30 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ * more_numbers - prints numbers 0 - 14 ten times
+ * Description: new line after each time
  */
-type name(args)
+void more_numbers(void)
 {
-	
-	return (val);
+	int i = 0, j;
+
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 14)
+		{
+			if (i < 10)
+			{
+				_putchar(j + '0');
+				j++;
+			}
+			else
+			{
+				_putchar('1');
+				_putchar((j % 10) + '0');
+				j++;
+			}
+		}
+		i++;
+		_putchar('\n');
+	}
 }
