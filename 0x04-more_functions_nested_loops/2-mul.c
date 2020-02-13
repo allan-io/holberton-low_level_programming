2645fb29e0e064f08eb2c0bbfc33ccd776b7ade6
allan-io <allanoliveira.dev@gmail.com>
2020-02-13 18:27:41 +0000

    Create 2-mul.c program

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ * mul - multiplies two numbers
+ * @a: int type number
+ * @b: int type number
+ * Return: value of a * b
  */
-type name(args)
+int mul(int a, int b)
 {
-	
-	return (val);
+	return (a * b);
 }
