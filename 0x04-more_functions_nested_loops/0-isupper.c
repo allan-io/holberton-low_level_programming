25da5f7a5a772fea079704749cbd6591b9b197c8
allan-io <allanoliveira.dev@gmail.com>
2020-02-13 16:14:07 +0000

    Create _isupper program

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,13 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ * _isupper - checks if char is a uppercase letter
+ * @c: is character to be checked
+ * Return: 1 if char is uppercase 0 otherwise
  */
-type name(args)
+int _isupper(int c);
 {
-	
-	return (val);
+	if (c >= 65 && c <= 90)	
+		return (1);
+	else
+		return(0);
 }
