944f9c86904f3679ee1b19a1693aafbb81a13f8f
allan-io <allanoliveira.dev@gmail.com>
2020-02-13 20:45:26 +0000

    Create 7-print_diagonal.c program

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,11 +1,27 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ * print_diagonal - draws a diagonal on the terminal
+ * @n: number of times \ should be printed
  */
-type name(args)
+void print_diagonal(int n)
 {
-	
-	return (val);
+	int i = 0, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	while (i < n)
+	{
+		j = 0;
+		while (j < i)
+		{
+			_putchar(' ');
+			j++;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		i++;
+	}
 }
