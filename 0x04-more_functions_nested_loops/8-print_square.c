83ca5d72e319e95a8f7a2a49918e4b13a2616c13
allan-io <allanoliveira.dev@gmail.com>
2020-02-14 04:45:00 +0000

    Fix code

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,21 +1,28 @@
 #include "holberton.h"
+
 /**
- * print_square - prints a square out of #s in the terminal
- * @size: size of each side of square
+ * print_square - print a square
+ * @size: size of square
+ * Description: Can only use _putchar to print. Use '#' to print square.
  */
 void print_square(int size)
 {
-	int i = 0, j;
+	int x, y;
 
-	while (i < size)
+	y = 0;
+
+	if (size < 1)
+		_putchar('\n');
+
+	while (y < size)
 	{
-		j = 0;
-		while (j < size)
+		x = 0;
+		while (x < size)
 		{
 			_putchar('#');
-			j++;
+			x++;
 		}
-		i++;
 		_putchar('\n');
+		y++;
 	}
 }
