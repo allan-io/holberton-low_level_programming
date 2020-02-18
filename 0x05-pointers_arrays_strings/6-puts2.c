2d6eb985f82d51249d804fe5fa25730bb2e700cf
allan-io <allanoliveira.dev@gmail.com>
2020-02-18 19:15:28 +0000

    Create 6-puts2.c program

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "holberton"
+/**
+ * puts2 - prints every other char of a string
+ * @str: string that will be printed
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
