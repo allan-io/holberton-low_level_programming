66d895e3a286427dcf10841a809d471f26524aab
allan-io <allanoliveira.dev@gmail.com>
2020-02-18 16:13:31 +0000

    Fix format warnings

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 /**
  * _strlen - returns length of given string
- * @*s: string variable (pointer to first character in array)
+ * @s: string variable (pointer to first character in array)
  * Return: string characters count;
  */
 int _strlen(char *s)
 {
 	int count = 0, i = 0;
-	
-	while(i != '\0')
+
+	while (i != '\0')
 	{
 		count++;
 		i++;
