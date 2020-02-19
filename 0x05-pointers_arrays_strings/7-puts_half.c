c2d4cf81a56c1c4d2719529dc256ce949eafbc68
allan-io <allanoliveira.dev@gmail.com>
2020-02-19 23:22:45 +0000

    Rerwite condition on for loop

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,26 +11,9 @@ void puts_half(char *str)
 	{
 		length++;
 	}
-
-	if (length % 2 == 0)
+	for (i /= 2; str[i] != '\0'; i++)
 	{
-		for (i = length / 2; i < length; i++)
-			_putchar(str[i]);
+		putchar(str[i]);
 	}
-	else
-	{
-		for (i = (length - 1) / 2; i < length; i++)
-		{
-			if (length - 1 == 0)
-			{
-				_putchar('\n');
-				return;
-			}
-			else
-			{
-				_putchar(str[i]);
-			}
-		}
-	}
-	_putchar('\n');
+	putchar('\n');
 }
