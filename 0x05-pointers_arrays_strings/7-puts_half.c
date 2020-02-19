ed6c7de651ffa305447badd78dfd282c377f966e
allan-io <allanoliveira.dev@gmail.com>
2020-02-19 22:41:18 +0000

    Create 7-puts_half.c program

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * puts_half - prints out half of the given string
+ * @str: value of string passed into function
+ */
+void puts_half(char *str)
+{
+	int length = 0, i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		length++;
+	}
+
+	if (length % 2 == 0)
+	{
+		for (i = length / 2; i < length; i++)
+			_putchar(str[i]);
+	}
+	else
+	{
+		for (i = (length - 1) / 2; i < length; i++)
+			_putchar(str[i]);
+	}
+}
