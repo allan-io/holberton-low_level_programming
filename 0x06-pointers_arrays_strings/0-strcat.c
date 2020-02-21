3a95f09c2c9f6af53df2be967ea8c82f00294b78
allan-io <allanoliveira.dev@gmail.com>
2020-02-21 02:59:14 +0000

    Initital commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: string that will be concatenated to
+ * @src: string toat will be concatenated to dest
+ * Return: pointer to string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+#!/bin/bash
+0x01 tasks on variables, if else statements, and while loops
+More pointers arrays and strings tasks
