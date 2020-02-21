7d7d5a976903a660d069d6d0c102b327121f51b1
allan-io <allanoliveira.dev@gmail.com>
2020-02-21 03:20:27 +0000

    changed variable

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,8 +12,8 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (i = 0; dest[i] != 0; i++)
 		;
-	for (j = 0; src[i] != 0 && n > 0; j++, i++, n--)
-		dest[i] = src[i];
+	for (j = 0; src[j] != 0 && n > 0; j++, i++, n--)
+		dest[i] = src[j];
 
 	return (dest);
 }
